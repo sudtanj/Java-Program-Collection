f7e2cc254985758ea5f7ccf7e5bbeff6771c8109
Sudono Tanjung <sudono007@gmail.com>
2016-12-02 20:34:13 +0700

    Add files via upload

---
+++ javacustom/src/EdgeDetector2D.java
@@ -0,0 +1,13 @@
+package customclass;
+
+import pencuriIkan.AIEnemy;
+
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public class EdgeDetector2D {
+	public static boolean isPixelCollide(BufferedImage image1,BufferedImage image2,Rectangle rect1, Rectangle rect2){
+		AIEnemy aiEnemy = new AIEnemy();
+		return false;
+	}
+}
